586d4bf57cc82599aa86884824378494e531be08
Althaf S <althaf.sabsara@gmail.com>
2023-11-01 00:41:35 +0530

    Merge conflict resolved

--- python_hw.py
+++ python_hw.py
@@ -10,51 +10,22 @@ def fizzbizz(n):
     else :
       print(i)
 
-<<<<<<< HEAD
-=======
-n = int(input("Enter any number to do operations : "))
-fizzbizz(n)
->>>>>>> remotes/origin/master
-
-
-#program to find whether provided string is palindrome
+#program to find whether given string is palindrome or not
 def palindrome(s):
-<<<<<<< HEAD
   return s == s[::-1]
-=======
-  w= ''
-  for i in s:
-    w = i + w
-  if w == s:
-    print("The string you provided is palindrome")
-  else :
-    print("The string you provided is not palindrome")
-    
-s = input("Enter your string to check if it is palindrome: ")
-palindrome(s)
->>>>>>> remotes/origin/master
 
-
-
-#program to find whether provided string is pangaram
+#program to find whether provided string is panagaram
 def panagram(s):
   c = 0
-<<<<<<< HEAD
-=======
-
-  
->>>>>>> remotes/origin/master
   s = s.lower()
   alphabets = "abcdefghijklmnopqrstuvwxyz"
   for i in alphabets: 
     if i in s:
       c += 1
   if c == len(alphabets):
-<<<<<<< HEAD
     return True
   return False
     
-
 
 #program to print the key value dictionary
 def freq(s):
@@ -65,35 +36,14 @@ def freq(s):
    else:
      dicti[i] = 1
  return dicti
- 
+
 
 if __name__ == "__main__":
    fizzbizz(n = int(input("Enter any number to do operations : ")))
    print(palindrome(s = input("Enter your string to check if it is palindrome: "))) 
    print(panagram(s = input("Enter your string here to check if it is panagram: ")))
    print(freq(s = input("Enter your string : ")))
- 
-=======
-    print("Is panagarm")
-  else :
-    print("Not pangarm")
-    
-s = input("Enter your string here to check if it is panagram: ")     
-panagram(s)
 
-#program to print the key value dictionary
-def freq(s):
- dict = {} 
- for i in s:
-   if i in dict:
-     dict[i] += 1
-   else:
-     dict[i] = 1
-  
- print(dict)
- 
-s = input("Enter your string : ")
-freq(s)
->>>>>>> remotes/origin/master
+
 
 
