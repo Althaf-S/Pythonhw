c5c0567fb540ce08790446d38b3449ec7a49be5b
Althaf S <althaf.sabsara@gmail.com>
2023-10-31 16:23:13 +0530

    Edit suggested for all the program has been done
    1) For programs provided with 'print' for output, all that have changed to 'return'.
    2) An import guard has been added to the program.
    3) Suggested changes for the program has been done and hence file has been edited..

--- python_hw.py
+++ python_hw.py
@@ -10,55 +10,44 @@ def fizzbizz(n):
     else :
       print(i)
 
-n = int(input("Enter any number to do operations : "))
-fizzbizz(n)
 
 
 #program to find whether provided string is palindrome
 def palindrome(s):
-  w= ''
-  for i in s:
-    w = i + w
-  if w == s:
-    print("The string you provided is palindrome")
-  else :
-    print("The string you provided is not palindrome")
-    
-s = input("Enter your string to check if it is palindrome: ")
-palindrome(s)
+  return s == s[::-1]
 
 
 
 #program to find whether provided string is pangaram
 def panagram(s):
   c = 0
-
-  
   s = s.lower()
   alphabets = "abcdefghijklmnopqrstuvwxyz"
   for i in alphabets: 
     if i in s:
       c += 1
   if c == len(alphabets):
-    print("Is panagarm")
-  else :
-    print("Not pangarm")
+    return True
+  return False
     
-s = input("Enter your string here to check if it is panagram: ")     
-panagram(s)
+
 
 #program to print the key value dictionary
 def freq(s):
- dict = {} 
+ dicti = {} 
  for i in s:
-   if i in dict:
-     dict[i] += 1
+   if i in dicti:
+     dicti[i] += 1
    else:
-     dict[i] = 1
-  
- print(dict)
+     dicti[i] = 1
+ return dicti
  
-s = input("Enter your string : ")
-freq(s)
+
+if __name__ == "__main__":
+   fizzbizz(n = int(input("Enter any number to do operations : ")))
+   print(palindrome(s = input("Enter your string to check if it is palindrome: "))) 
+   print(panagram(s = input("Enter your string here to check if it is panagram: ")))
+   print(freq(s = input("Enter your string : ")))
+ 
 
 
